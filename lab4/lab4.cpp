8dfe0c0bc6ef2b8645e36597524b24bd89598397
gregdumb <gregoryjb@rocketmail.com>
2015-10-13 23:15:16 -0700

    :)

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -2,6 +2,7 @@
 #include <iomanip>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 #include "wine.h"
 
@@ -43,8 +44,22 @@ Wine getWineInput()
 	return newWine;
 }
 
-void printAllWines(vector<Wine> wines)
+// Sort functions used by the vector to sort itself
+bool sortByPrice(Wine left, Wine right)
 {
+	return left.getPrice() < right.getPrice();
+}
+bool sortByRating(Wine left, Wine right)
+{
+	return left.getRating() > right.getRating();
+}
+
+void printAllWines(vector<Wine> wines, int ratingMin = 0, int ratingMax = 99, int priceMin = 0, int priceMax = 100)
+{
+	// Sort the wines
+	sort(wines.begin(), wines.end(), sortByPrice); // @TODO &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& let user select sort type and bounds
+	
+	// Variables that control the formatting of output
 	int nameWidth = 10;
 	int vintageWidth = 10;
 	int ratingWidth = 10;
@@ -55,9 +70,9 @@ void printAllWines(vector<Wine> wines)
 	// Make a bar to use for the ends
 	string bar = "";
 	bar.append(totalWidth + 4, '-');
-		
-	cout << bar << endl;
 	
+	// Print column labels
+	cout << bar << endl;
 	cout << "| ";
 	cout << setw(nameWidth) << left << "Name" 
 	     << setw(vintageWidth) << "Vintage"
@@ -66,17 +81,41 @@ void printAllWines(vector<Wine> wines)
 	     << setw(winearyWidth) << "Wineary" 
 	     << " |" << endl;
 	
+	// This will keep track of how many wines make the cut
+	int numSelectedWines = 0;
+	
+	// These will keep track of selected prices
+	double totalPrice = 0;
+	double avgPrice = 0;
+	
+	// Print the wine, if it makes the cut
 	for(int i = 0; i < wines.size(); i++)
 	{
+		// IF rating and price are within set bounds
+		if(wines[i].getRating() <= ratingMax && wines[i].getRating() >= ratingMin
+		&& wines[i].getPrice() <= priceMax && wines[i].getPrice() >= priceMin)
+		{		
 		cout << "| ";
 		cout << setw(nameWidth) << left << wines[i].getName()
 		     << setw(vintageWidth) << wines[i].getVintage()
 		     << setw(ratingWidth) << wines[i].getRating()
-		     << setw(priceWidth) << wines[i].getPrice()
+		     << "$" << setw(priceWidth-1) << fixed << setprecision(2) << wines[i].getPrice() // We have to subtract width by 1 to make space for the dollar sign
 		     << setw(winearyWidth) << wines[i].getWineary()
 		     << " |" << endl;
+		    
+		 // Update our selected wine statistics
+		 numSelectedWines++;
+		 totalPrice += wines[i].getPrice();
+		 }
 	}
 	
+	// Calculate the average
+	avgPrice = totalPrice / numSelectedWines;
+	
+	// Print results
+	cout << bar << endl;
+	cout << "| " << "Total wines selected: " << numSelectedWines << endl;
+	cout << "| " << "Average wine price: $" << avgPrice << endl;
 	cout << bar << endl;
 }
 
